daaae54bacf39d2c043f808398f3706da5115d5c
Chris <hakizachrist1@gmail.com>
2024-04-04 16:42:26 +0200

    Update customer

--- app/models/customer_order.rb
+++ app/models/customer_order.rb
@@ -1,2 +1,6 @@
 class CustomerOrder < ApplicationRecord
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :amount_ordered, presence: true, numericality: {only_integer: true, greater_than:0}
+    validates :phoneNumber, presence: true
 end
