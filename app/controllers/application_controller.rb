cb386546612cf3f75aa53a0298cb220af662a20f
Chris <hakizachrist1@gmail.com>
2024-04-24 21:59:56 +0200

    Update application_Controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password password_confirmation])
+      end
+
+      protected
+      def after_sign_in_path_for(resource)
+        if resource.is_a?(User)
+        user_customer_orders_path(resource)
+        end 
+      end 
+      def  after_sign_up_path_for(resource)
+        if resource.is_a?(User)
+        new_user_session_path(resource)
+        end 
+      end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  root 'customer_orders#index'
+ 
+  devise_for :users
+  root 'users#index'
 
-  resources :customer_orders
+  resources :users, only: [:index] do
+    resources :customer_orders
+  end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
